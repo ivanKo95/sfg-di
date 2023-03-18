6eb535be70f9a93fb882a434d01c7fa561541d7d
ivan.kolbukh <ivan.kolbukh@vitechteam.com>
2023-03-19 00:06:28 +0200

    Refactored code.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -11,31 +11,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
-		var run = SpringApplication.run(SfgDiApplication.class, args);
+  public static void main(String[] args) {
+    var run = SpringApplication.run(SfgDiApplication.class, args);
 
-		MyController myController = (MyController) run.getBean("myController");
+    var myController = (MyController) run.getBean("myController");
 
-		System.out.println("---Profile");
-		I18NController i18NController = (I18NController) run.getBean("i18NController");
-		System.out.println(i18NController.sayHello());
+    System.out.println("---Profile");
+    var i18NController = (I18NController) run.getBean("i18NController");
+    System.out.println(i18NController.sayHello());
 
-		System.out.println("--- Primary");
-		System.out.println(myController.sayHello());
+    System.out.println("--- Primary");
+    System.out.println(myController.sayHello());
 
-		System.out.println("___Property");
-		PropertyInjectedController propertyInjectedController =
-				(PropertyInjectedController) run.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
+    System.out.println("___Property");
+    var propertyInjectedController = (PropertyInjectedController) run.getBean("propertyInjectedController");
+    System.out.println(propertyInjectedController.getGreeting());
 
-		System.out.println("___Setter");
-		SetterInjectedController setterInjectedController =
-				(SetterInjectedController) run.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
+    System.out.println("___Setter");
+    var setterInjectedController = (SetterInjectedController) run.getBean("setterInjectedController");
+    System.out.println(setterInjectedController.getGreeting());
 
-		System.out.println("___Constructor");
-		ConstructorInjectedController constructorInjectedController =
-				(ConstructorInjectedController) run.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
-	}
+    System.out.println("___Constructor");
+    var constructorInjectedController = (ConstructorInjectedController) run.getBean("constructorInjectedController");
+    System.out.println(constructorInjectedController.getGreeting());
+  }
 }
--- src/test/java/guru/springframework/sfgdi/controller/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfgdi/controller/PropertyInjectedControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PropertyInjectedControllerTest {
-
   private PropertyInjectedController propertyInjectedController;
 
   @BeforeEach
