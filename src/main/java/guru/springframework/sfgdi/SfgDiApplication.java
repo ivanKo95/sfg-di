4d3cb9ec2defbf46361fd7aea1b41ac277c117a8
ivan.kolbukh <ivan.kolbukh@vitechteam.com>
2023-03-19 12:33:57 +0200

    Test10.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfgDiApplication {
+
   public static void main(String[] args) {
     var run = SpringApplication.run(SfgDiApplication.class, args);
 
