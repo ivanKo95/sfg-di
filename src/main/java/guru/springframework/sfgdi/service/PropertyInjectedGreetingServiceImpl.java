a9a1f6e1b8239362d19102a0fdb6006c30185afd
ivan.kolbukh <ivan.kolbukh@vitechteam.com>
2023-03-26 01:13:00 +0200

    test1

--- src/main/java/guru/springframework/sfgdi/service/PropertyInjectedGreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/service/PropertyInjectedGreetingServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 public class PropertyInjectedGreetingServiceImpl implements GreetingService {
   @Override
   public String sayGreeting() {
-    return "Hello World - Property";
+    String a = "Property";
+    return String.format("Hello World - %s", a);
   }
 }
