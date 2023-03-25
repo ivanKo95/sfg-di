f71bc0f970cbcc54d15965dd457bd639c9fccec0
ivan.kolbukh <ivan.kolbukh@vitechteam.com>
2023-03-26 01:33:05 +0200

    test5

--- src/main/java/guru/springframework/sfgdi/service/PropertyInjectedGreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/service/PropertyInjectedGreetingServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PropertyInjectedGreetingServiceImpl implements GreetingService {
+
   @Override
   public String sayGreeting() {
     String a = "Property";
